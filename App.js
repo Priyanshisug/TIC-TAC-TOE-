b1b304caca96ae9b6af4d92bd2ba9168735207e6
Priyanshisug <124687429+Priyanshisug@users.noreply.github.com>
2024-08-17 22:09:51 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import TicTacToe from './components/Tic-Tac-Toe/Tic-Tac-Toe';
+
+function App() {
+  return (
+    <div>
+     <TicTacToe/>
+      
+       
+    </div>
+  );
+}
+
+export default App;
